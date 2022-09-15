8d34678e141d856cded8222a705c38470b90bfe8
Jaron <jaronchretien@yahoo.com>
2022-09-15 11:25:35 -0500

    Moving Snake

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+//KEYCODES FOR THE ARROW KEYS
+//left = 37
+//up = 38
+//right = 39
+//down = 40
+// document.onkeydown = function keyPress(e)  
+// {
+//   if (e.keyCode === 38) {
+//     console.log('up arrow pressed')
+//   } else if (e.keyCode === 40) {
+//     console.log('down arrow pressed')
+//   } else if (e.keyCode === 37) {
+//     console.log('left arrow pressed')
+//   } else if (e.keyCode === 39) {
+//     console.log('right arrow pressed')
+//   }
+// }
+
+
+
+
 //RENDER TIME
-let lastRenderTime = 0
 
-const SNAKE_SPEED = 1
-function main(currentTime) {
-    window.requestAnimationFrame(main)
-    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondSinceLastRender <1 / SNAKE_SPEED) return
-
-    lastRenderTime = currentTime
-    console.log(secondSinceLastRender)
-}
-
-window.requestAnimationFrame(main)
 
 
 
@@ -34,7 +43,7 @@ submitButton.addEventListener('click', function nameSelect () {
     
     inputField.className = "hidden"
     
-    //CREATE START BUTTON 
+    //PREVIOUSLY DEFINED
     let startButton = document.createElement("button")
     
     startButton.innerText = "Start!"
@@ -48,17 +57,20 @@ submitButton.addEventListener('click', function nameSelect () {
 })
 
 
+let initialState = {
 
+}
 
 //Game State
 let gameState = {
     apple: [11, 8],
     snake: {
-        body : [ [10, 5], [10, 6], [10, 7], [10, 8] ],
+        body : [ [10, 5], [10, 6], [10, 7], [10, 8]],
         nextDirection: [1, 0]
     }
   }
- 
+
+
 
 // Creating the Board Element//
   let boardElm = document.querySelector("#grid")
@@ -88,8 +100,45 @@ let gameState = {
 //Default Score
 document.getElementById('currentScore').innerText =  `Current: 0`
 document.getElementById('bestScore').innerText = `Best: 0`
+
+
+
+
+
+
+
+
 //Create Snake
-//End of Initial State
+let snakeBody = gameState.snake.body
+
+let lastRenderTime = 0
+
+const SNAKE_SPEED = 1
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondSinceLastRender <1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+    console.log(secondSinceLastRender)
+    updateSnake ()
+    drawSnake ()
+    
+    
+    
+}
+
+window.requestAnimationFrame(main)
+
+
+
+
+
+//Update Snake Funciton
+//Want each previous array to equal the array after it.
+//Grab Each index of an array and equal it to the next
+//Add 1 to the indexes
 
 
 
@@ -97,28 +146,21 @@ document.getElementById('bestScore').innerText = `Best: 0`
 
 
 
+function updateSnake () {
+    for (let b = snakeBody.length - 2; b >= 0; b--){
+        snakeBody[b + 1] = snakeBody[b]
+        
+    }
+    snakeBody[0][0] += 0
+    snakeBody[0][1] += 1
+    
+    
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function drawSnake () {
+    snakeBody.forEach(segment => boardElm.children[segment[0]].children[segment[1]].className = 'snake');
+}
 
 
